229ef83f4a8c35bfd32a75015aea606f18f574f0
nup oniichan <nuponiibaka.com@gmail.com>
2023-03-03 11:40:32 +0700

    add my file

--- UI/UIInventoryItem.cs
+++ UI/UIInventoryItem.cs
@@ -1,18 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Presets;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIInventoryItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TMP_Text quantityText;
+    [SerializeField] private Image border;
+    public event Action<UIInventoryItem> OnItemClick, OnItemDropOn, OnItemBeginDrag, OnItemEndDrag, OnRightMouseBtnClick;
+    private bool empty = false;
+
+    public void Awake()
     {
-        
+        ResetData();
+        DeSelect();
+    }
+    public void ResetData()
+    {
+        this.itemImage.gameObject.SetActive(false);
+        empty = true;
+    }
+    public void DeSelect()
+    {
+        border.enabled = false;
+    }
+    public void SetData(Sprite sprite, int quantity)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityText.text = quantity + "";
+        empty = false;
+    }
+    public void Select()
+    {
+        border.enabled = true;
+    }
+    public void OnBeginDrag()
+    {
+        if (empty)
+            return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+    public void OnEndDrag()
+    {
+        OnItemEndDrag?.Invoke(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrop()
     {
-        
+        OnItemDropOn?.Invoke(this);
+    }
+    public void OnEndDrop()
+    {
+        OnItemDropOn?.Invoke(this);
+    }
+    public void OnPointerClick(BaseEventData data)
+    {
+        if (empty)
+            return;
+        PointerEventData pointerData = (PointerEventData)data;
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClick?.Invoke(this);
+        }
     }
 }
--- UI/UIInventoryPage.cs
+++ UI/UIInventoryPage.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class UIInventoryPage : MonoBehaviour
 {
@@ -14,8 +15,39 @@ public class UIInventoryPage : MonoBehaviour
             UIInventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
             uiItem.transform.SetParent(contentpanel);
             ListOfItems.Add(uiItem);
+            uiItem.OnItemClick += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDropOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
         }
     }
+
+    private void HandleShowItemActions(UIInventoryItem obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleEndDrag(UIInventoryItem obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleSwap(UIInventoryItem obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleBeginDrag(UIInventoryItem obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleItemSelection(UIInventoryItem obj)
+    {
+        throw new NotImplementedException();
+    }
+
     public void Show()
     {
         gameObject.SetActive(true);
